7ee4d562e3d4163e4f4e15fe9731ff16400f59c0
Bandi Krupa Bhavani <bandikrupabhavani@gmail.com>
2025-09-14 20:55:20 +0530

    update

--- library management/firebase-config.js
+++ library management/firebase-config.js
@@ -1,5 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyALtm-aCvAq5FmKygk20lJozGjPHe2vrDw",
@@ -13,3 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
