01f28c5809517c56117dcaaadf91141ace40893d
Bandi Krupa Bhavani <bandikrupabhavani@gmail.com>
2025-09-17 12:25:58 +0530

    profile

--- library management/firebase-config.js
+++ library management/firebase-config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyALtm-aCvAq5FmKygk20lJozGjPHe2vrDw",
   authDomain: "capstone-project-5b469.firebaseapp.com",
   projectId: "capstone-project-5b469",
-  storageBucket: "capstone-project-5b469.firebasestorage.app",
+  storageBucket: "capstone-project-5b469.appspot.com",
   messagingSenderId: "681124702142",
   appId: "1:681124702142:web:c69e0f1e7b4b083b227bef",
   measurementId: "G-EDCKMQ0T9C"
